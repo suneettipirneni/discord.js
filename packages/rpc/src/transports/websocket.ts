4b0c1a6c030435e2667f0f2450f41407ca2f0ac2
suneettipirneni <suneettipirneni@icloud.com>
2022-07-17 21:08:50 -0400

    feat(rpc): initial commit

---
+++ packages/rpc/src/transports/websocket.ts
@@ -0,0 +1,86 @@
+import EventEmitter from 'events';
+import type { default as WebSocketType } from 'ws';
+import type { Transport } from './index';
+import type { RPCClient } from '../client';
+import { browser } from '../constants';
+
+type WebSocketConstructor = new (url: string, options?: WebSocketType.ClientOptions) => WebSocketType;
+
+// @ts-expect-error
+// eslint-disable-next-line
+const WebSocket = (browser ? window.WebSocket : require('ws')) as WebSocketConstructor;
+
+const pack = (d: Record<string, unknown> | string) => JSON.stringify(d);
+const unpack = (s: string) => JSON.parse(s) as Record<string, unknown>;
+
+class WebSocketTransport extends EventEmitter implements Transport {
+	private readonly client: RPCClient;
+	private ws: WebSocketType | null;
+	private tries: number;
+
+	public constructor(client: RPCClient) {
+		super();
+		this.client = client;
+		this.ws = null;
+		this.tries = 0;
+	}
+
+	public connect() {
+		const port = 6463 + (this.tries % 10);
+		this.tries += 1;
+
+		this.ws = new WebSocket(
+			`ws://127.0.0.1:${port}/?v=1&client_id=${this.client.clientId!}`,
+			browser ? undefined : { origin: this.client.options.origin },
+		);
+		this.ws.onopen = this.onOpen.bind(this);
+		this.ws.onclose = this.onClose.bind(this);
+		this.ws.onerror = this.onError.bind(this);
+		this.ws.onmessage = this.onMessage.bind(this);
+	}
+
+	public onOpen() {
+		this.emit('open');
+	}
+
+	public onClose(event: WebSocketType.CloseEvent) {
+		if (!event.wasClean) {
+			return;
+		}
+		this.emit('close', event);
+	}
+
+	public onError(event: WebSocketType.ErrorEvent) {
+		try {
+			this.ws!.close();
+		} catch {} // eslint-disable-line no-empty
+
+		if (this.tries > 20) {
+			this.emit('error', event.error);
+		} else {
+			setTimeout(() => {
+				this.connect();
+			}, 250);
+		}
+	}
+
+	public onMessage(event: WebSocketType.MessageEvent) {
+		this.emit('message', unpack(event.data as string));
+	}
+
+	public send(data: Record<string, unknown>) {
+		this.ws!.send(pack(data));
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-empty-function
+	public ping() {}
+
+	public close() {
+		return new Promise<void>((r) => {
+			this.once('close', r);
+			this.ws!.close();
+		});
+	}
+}
+
+module.exports = WebSocketTransport;
---
+++ packages/rpc/src/util.ts
@@ -0,0 +1,47 @@
+let register;
+try {
+	// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
+	const { app } = require('electron');
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+	register = app.setAsDefaultProtocolClient.bind(app);
+} catch (err) {
+	try {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports
+		register = require('register-scheme');
+	} catch (e) {} // eslint-disable-line no-empty
+}
+
+if (typeof register !== 'function') {
+	register = () => false;
+}
+
+function pid() {
+	if (typeof process !== 'undefined') {
+		return process.pid;
+	}
+	return null;
+}
+
+const uuid4122 = () => {
+	let uuid = '';
+	for (let i = 0; i < 32; i += 1) {
+		if (i === 8 || i === 12 || i === 16 || i === 20) {
+			uuid += '-';
+		}
+		let n;
+		if (i === 12) {
+			n = 4;
+		} else {
+			const random = (Math.random() * 16) | 0;
+			if (i === 16) {
+				n = (random & 3) | 0;
+			} else {
+				n = random;
+			}
+		}
+		uuid += n.toString(16);
+	}
+	return uuid;
+};
+
+export { pid, uuid4122 as uuid, register };
