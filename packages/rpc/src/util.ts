a4d112d88eef413625593465ff4d4122cfc96514
Micah Benac <micahbenac52@gmail.com>
2022-08-13 19:27:08 -0400

    chore: setup type for register util

--- packages/rpc/src/util.ts
+++ packages/rpc/src/util.ts
@@ -1,18 +1,14 @@
-let register;
+let register: (id: string) => void = () => false;
 try {
-	// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 	const { app } = require('electron');
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
 	register = app.setAsDefaultProtocolClient.bind(app);
 } catch (err) {
 	try {
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports
 		register = require('register-scheme');
 	} catch (e) {} // eslint-disable-line no-empty
-}
-
-if (typeof register !== 'function') {
-	register = () => false;
 }
 
 function pid() {
