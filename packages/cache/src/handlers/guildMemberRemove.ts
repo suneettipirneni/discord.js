b0143e2bb6f53dfd0661c43a3ec53893548b5f4a
suneettipirneni <suneettipirneni@icloud.com>
2022-12-13 10:25:25 -0500

    fix: account for users that may not share guilds with the bot

--- packages/cache/src/handlers/guildMemberRemove.ts
+++ packages/cache/src/handlers/guildMemberRemove.ts
@@ -5,4 +5,12 @@ export function handleGuildMemberRemove(cache: Cache, data: GatewayGuildMemberRe
 	const { guild_id, user } = data;
 	cache.members.delete(user.id);
 	cache.guildMembers.get(guild_id)?.delete(user.id);
+
+	// Check if member still shares another guild with the bot
+	for (const guild of cache.guilds.keys()) {
+		if (cache.guildMembers.get(guild)?.has(user.id)) return;
+	}
+
+	// User no longer shares any guilds with the bot, delete user from cache
+	cache.users.delete(user.id);
 }
